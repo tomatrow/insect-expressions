d52d0f2a75d7bdb9788c913b4481307061790202
AJ Caldwell <tomatrow@gmail.com>
2021-06-09 21:30:57 -0700

    Implement the collections page pretty well

--- src/routes/collections/[handle].svelte
+++ src/routes/collections/[handle].svelte
@@ -23,18 +23,19 @@
     export let products: ProductConnection
 </script>
 
-<h1>{title}</h1>
-{@html descriptionHtml}
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+<h1 class="text-dark pt-4 text-4xl">{title}</h1>
+<div class="text-dark space-y-2 my-4">{@html descriptionHtml}</div>
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mb-16 text-black">
     {#each products.edges.map(edge => edge.node) as { title, images, handle, priceRange }}
-        <a href="/products/{handle}">
+        <a href="/products/{handle}" class="space-y-2">
             <img
                 class="w-full h-auto"
                 src={images.edges[0].node.originalSrc}
                 alt={images.edges[0].node.altText}
             />
-            <h2 class="text-center">{title}</h2>
-            <span>Base Price: {formatMoney(priceRange.minVariantPrice.amount)}</span>
+            <h2 class="text-center font-bold">{title}</h2>
+            <span class="text-sm">Base Price: {formatMoney(priceRange.minVariantPrice.amount)}</span
+            >
         </a>
     {/each}
 </div>
