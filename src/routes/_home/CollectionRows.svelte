27d89df09ade8915c992611670d4f4d9fa75308f
AJ Caldwell <tomatrow@gmail.com>
2022-05-15 20:16:58 -0700

    Use webp

--- src/routes/_home/CollectionRows.svelte
+++ src/routes/_home/CollectionRows.svelte
@@ -6,14 +6,14 @@
     title="A Hidden World"  
     reverse
     class="my-4 sm:my-0"
-    image={{ src: "/images/home/a-hidden-world.jpg" }}
+    image={{ src: "/images/home/a-hidden-world.webp" }}
     >
     Leaf and stick mimicking insects camouflage among a scattering of realistic plants on the forest floor in this insect art piece. These masters of disguse beckon you into their hidden world deep in the woods, where the smell of fallen leaves and the stillness of nature envelope you in peace.
 </CollectionRow>
 
 <CollectionRow 
     title="Dazzling Deception"  
-    image={{ src: "/images/home/dazzling-owl.jpg" }}
+    image={{ src: "/images/home/dazzling-owl.webp" }}
     class="my-4 sm:my-0"
     >
     Unblinking eyes seem to follow your every move as an owl peers out from the branches, but it isn't and owl; it's the butterfly displayed in this insect art piece posing against a backdrop of realistic tree bark.
@@ -23,7 +23,7 @@
     title="Impressive Proportions"  
     reverse
     class="my-4 sm:my-0"
-    image={{ src: "/images/home/impressive-proportions.jpg" }}
+    image={{ src: "/images/home/impressive-proportions.webp" }}
     >
     A beetle sits peacefully on a fallen branch enjoying the tranquility of the silent forest. Enclosed in an acrylic display case or glass dome, this natural insect art piece celebrates the awe-inspiring proportions of giant beetle species.
 </CollectionRow>
--- src/routes/_home/Hero.svelte
+++ src/routes/_home/Hero.svelte
@@ -3,7 +3,7 @@
 </script>
 
 
-<section class="bg-[position:center_65%] bg-cover bg-no-repeat h-[36rem] flex items-center justify-center" style:background-image="url(/images/home/shelf-beetle.jpg)" >
+<section class="bg-[position:center_65%] bg-cover bg-no-repeat h-[36rem] flex items-center justify-center" style:background-image="url(/images/home/shelf-beetle.webp)" >
     <article class="w-full max-w-lg bg-white/50 flex items-center justify-center gap-4 p-4 flex-col sm-mx-4">
         <h1 class="text-2xl font-bold tracking-wide text-center">Handcraft Insect Art Décor</h1>
         <Link secondary blob href="/collections">Explore Collections</Link>
